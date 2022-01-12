25c955a39d530510a6f9f0436ffb2aa3cbc90935
Kanaishk Garg <kanaishkgarg@gmail.com>
2022-01-12 10:01:09 +0530

    Changing database

--- server.js
+++ server.js
@@ -31,5 +31,5 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.listen(process.env.PORT || 3000, ()=> {
-  console.log('app is running on port ${process.env.PORT}');
+  console.log('app is running on port ${ process.env.PORT }');
 })
